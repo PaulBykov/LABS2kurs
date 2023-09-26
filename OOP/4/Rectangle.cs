5142e1e14d7212501d486f35de43232f08c8b36a
Paul <fleinike2@gmail.com>
2023-09-26 23:30:31 +0300

    v 1.1

---
+++ OOP/4/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    internal class Rectangle:Figure, IFiguresInterface
+    {
+        private int width;
+        private int height;
+
+        Rectangle(int width, int height)
+        {   
+            this.width = width;
+            this.height = height;
+        }
+
+        public void input(int x, int y) { 
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void resize(int width, int height) { 
+            this.width = width;
+            this.height = height;
+        }
+
+
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+    }
+}
